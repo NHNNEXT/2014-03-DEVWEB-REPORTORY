ae66d0c604e59c59a9fc24f77793449f7cdec7a6
Byeol <byeol@hotmail.co.kr>
2014-12-30 17:40:01 +0900

    Improvement user controller
    Integrate student and professor login.

--- SERVER/src/main/java/controllers/UserController.java
+++ SERVER/src/main/java/controllers/UserController.java
@@ -17,6 +17,7 @@ import util.JsonDataSerializable;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -27,74 +28,76 @@ import java.util.function.Supplier;
 @Controller
 public class UserController {
 
-    //public static PasswdEncrypter
+    // TODO: 로그인은 교수와 학생이 통합되어야 한다.
+    // 로그인 정보는 교수와 학생과 무관하게 unique 하다.
 
-    @POST("/professor/login")
-    public static Result profLogin(Request req) throws SQLException {
-
-        String email = req.body().asFormUrlEncoded().getParam("email");
-        String passwd = req.body().asFormUrlEncoded().getParam("passwd");//todo
-
-        if(email==null || passwd==null)
-            return Result.BadRequest.plainText("input parameters");
-
-        List<ProfessorUser> profList = ProfessorUserJoin.getQuery()
-                .where((t)->(t.email) .isEqualTo (email) .and
-                        ((t.passwd) .isEqualTo  (passwd)))
-                .list(req.getDBConnection());
-
-        if(profList.isEmpty())
-            return Result.Forbidden.plainText("no such professor");
-
-        ProfessorUser logindata = profList.get(0);
-
-        return Result.Ok.plainText("logined with "+logindata.email)
-                .withNewSession(
-                        new SessionData<String>("userType","prof"),
-                        new SessionData<String>("loginData",logindata.serialize())
-                );
-    }
-
-
-    @POST("/student/login")
-    public static Result stuLogin(Request req) throws SQLException {
+    @POST("/login")
+    public static Result login(Request req) throws SQLException {
 
         String email = req.body().asFormUrlEncoded().getParam("email");
         String passwd = req.body().asFormUrlEncoded().getParam("passwd");//todo
 
+        System.out.println("email: " + email);
+        System.out.println("passwd: " + passwd);
+
         if(email==null || passwd==null)
             return Result.BadRequest.plainText("input parameters");
 
-        List<StudentUser> profList = StudentUserJoin.getQuery()
+        User user = UserTable.getQuery()
                 .where((t)->(t.email) .isEqualTo (email) .and
                         ((t.passwd) .isEqualTo  (passwd)))
-                .list(req.getDBConnection());
+                .first(req.getDBConnection());
 
-        if(profList.isEmpty())
-            return Result.Forbidden.plainText("no such student");
+        if(user == null)
+            return Result.Forbidden.plainText("no such user");
 
-        StudentUser logindata = profList.get(0);
+        List<JsonDataSerializable> userList = new ArrayList<>();
 
-        return Result.Ok.plainText("logined with "+logindata.email)
+        ProfessorUser professorUser = ProfessorUserJoin.getQuery()
+                .where((t)->(t.uid) .isEqualTo( (user.uid)))
+                .first(req.getDBConnection());
+
+        userList.add(professorUser);
+
+        StudentUser studentUser = StudentUserJoin.getQuery()
+                .where((t)->(t.uid) .isEqualTo( (user.uid)))
+                .first(req.getDBConnection());
+
+        userList.add(studentUser);
+
+        String userType = "";
+
+        if(userList.get(0) instanceof ProfessorUser) {
+            userType = "prof";
+        }
+
+        if(userList.get(0) instanceof StudentUser) {
+            userType = "stu";
+        }
+
+        return Result.Ok.plainText("logined with "+user.email)
                 .withNewSession(
-                        new SessionData<String>("userType","stu"),
-                        new SessionData<String>("loginData",logindata.serialize())
+                        new SessionData<String>("userType",userType),
+                        new SessionData<String>("loginData",userList.get(0).serialize())
                 );
     }
 
-    @POST("/professor/register")
+    // TODO: 가입은 professor/student (collection)에 POST method로 대체되어야 한다.
+    // REST URI에 register 라는 동사를 사용하는 것은 의미를 모호하게 한다.
+
+    @POST("/professor")
     public static Result profRegister(Request req) throws SQLException {
         ProfessorUser record = req.body().asJson().mapping(ProfessorUser.class);
         return userRegister(req,record.toUser(),(id)->{
                     Professor prof = record.toProfessor();
                     prof.uid = id;
 
-                    ProfessorTable.getQuery()
-                            .insert(req.getDBConnection(), new Professor[]{prof});
+            ProfessorTable.getQuery()
+                    .insert(req.getDBConnection(), new Professor[]{prof});
                 });
     }
 
-    @POST("/student/register")
+    @POST("/student")
     public static Result stuRegister(Request req) throws SQLException {
         StudentUser record = req.body().asJson().mapping(StudentUser.class);
         return userRegister(req, record.toUser(), (id) -> {
@@ -109,7 +112,7 @@ public class UserController {
         record.passwd = record.passwd;//todo
         record.join_date = new Timestamp(System.currentTimeMillis());
         return UserTable.getQuery()
-                        .insertRetunningGenKey(req.getDBConnection(), new User[]{record})
+                .insertRetunningGenKey(req.getDBConnection(), new User[]{record})
                         .get(0);
     }
 
